b4b97012e889671fec7825f4e1cfb7a27a61e9f5
bjchiong <benjamin.chiong@griffithuni.edu.au>
2017-07-23 13:33:22 +0000

    set up

---
+++ hello-world.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+  <link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+  <link href="/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+  <!--[if IE]>
+      <link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+  <![endif]-->
+</head>
+<body>
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+echo 'Hello world from Cloud9!';
+
+?>
+</body>
+</html>
