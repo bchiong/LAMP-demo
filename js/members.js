4323469007783ce5cee62d6ce85fe328e58943d5
bjchiong <benjamin.chiong@griffithuni.edu.au>
2017-09-14 11:57:05 +0000

    fix member double up

--- js/members.js
+++ js/members.js
@@ -1,41 +1,84 @@
+$(document).ready(function() {
 
-$('#submit-member-form').click(function(){
-//   alert('Hello world');
-        var firstname = $('.fname').val();
-        var surname = $('.sname').val();
-        var email = $('.email').val();
-        var details = {'fname=':firstname, '&sname=':surname, '&email=':email};
-        // var dataString = 'firstname=' + firstname + '&surname=' + surname + '&email=' + email;
-        var dataString = ''
-        for (var key in details) {
-            if (details[key] !== '') {
-                dataString += key + details[key];
-            } 
+    /* Bar graph members */
+    $.ajax({
+        type: "GET",
+        url: "members-signup.php",
+        cache: false,
+        success: function(data) {
+            var graph = [];
+            for (var i = 0; i < data.length; i++) {
+                var bar = [];
+                bar.push(data[i].signups);
+                bar.push(data[i].year);
+                bar.push('#f3f3f3');
+                graph.push(bar);
+            }
+            $('#divForGraph').jqBarGraph({
+                data: graph,
+                width: 800
+            });
         }
-   
-      
-   console.log(dataString);
-//   console.log(dataString2 + '2');
+    });
+
+    /* Display all members */
+    setTimeout(function() {
+        $("table").find("tr:gt(0)").remove();
         $.ajax({
             type: "GET",
-            url: "members.php",
-            data: dataString,
+            url: "members-all.php",
             cache: false,
-            success: function (data) {
-                // var obj = JSON.stringify(data);
-                // $('search-result-table').text('')
-                console.log(data);
-                
-        //         var tr;
-        // for (var i = 0; i < data.length; i++) {
-        //     tr = $('<tr/>');
-        //     tr.append("<td>" + data[i].firstname + "</td>");
-        //     tr.append("<td>" + data[i].surname + "</td>");
-        //     tr.append("<td>" + data[i].email + "</td>");
-        //     $('table').append(tr);
-        // }
+            success: function(data) {
+                var tr;
+                for (var i = 0; i < data.length; i++) {
+                    tr = $('<tr/>');
+                    tr.append("<td>" + data[i].firstname + "</td>");
+                    tr.append("<td>" + data[i].surname + "</td>");
+                    tr.append("<td>" + data[i].email + "</td>");
+                    $('table').append(tr);
+                }
             }
-            
         });
-        
-}); 
+    }, 1500);
+
+});
+
+
+/* Search member */
+$('#submit-member-form').click(function() {
+    $("table").find("tr:gt(0)").remove();
+    var firstname = $('.fname').val();
+    var surname = $('.sname').val();
+    var email = $('.email').val();
+    var details = {
+        'fname=': firstname,
+        '&sname=': surname,
+        '&email=': email
+    };
+
+    var dataString = ''
+    for (var key in details) {
+        if (details[key] !== '') {
+            dataString += key + details[key];
+        }
+    }
+
+    $.ajax({
+        type: "GET",
+        url: "members.php",
+        data: dataString,
+        cache: false,
+        success: function(data) {
+            var tr;
+            for (var i = 0; i < data.length; i++) {
+                tr = $('<tr/>');
+                tr.append("<td>" + data[i].firstname + "</td>");
+                tr.append("<td>" + data[i].surname + "</td>");
+                tr.append("<td>" + data[i].email + "</td>");
+                $('table').append(tr);
+            }
+        }
+
+    });
+
+});
--- members.php
+++ members.php
@@ -3,20 +3,14 @@ include('connection.php');
 include('functions.php');
 
 // Encode to JSON
-$firstname = $_GET['fname'];
-$surname = $_GET['sname'];
-$email = $_GET['email'];
-  $firstname = htmlspecialchars($firstname);
-  $surname = htmlspecialchars($surname);
-  $email = htmlspecialchars($email);
-    // collect value of input field
-    // echo $firstname;
-    // echo $surname;
-    // echo $email;
+$firstname = htmlspecialchars($_GET['fname']);
+$surname = htmlspecialchars($_GET['sname']);
+$email = htmlspecialchars($_GET['email']);
+
 if (empty($_GET)) {
     echo "Fill in name or email";
    } else {
-    $sql = "SELECT firstname, surname, email from members where firstname = '$firstname' or surname = '$surname' or email = '$email'";
+    $sql = "select firstname, surname, email from members where firstname = '$firstname' or surname = '$surname' or email = '$email'";
     $result = mysqli_query($db, $sql);
     if ($result->num_rows > 0) {
     // output data of each row
@@ -26,7 +20,7 @@ if (empty($_GET)) {
         header('Content-Type: application/json');
         echo json_encode($return_val);
     } else {
-        echo "0 results";
+        echo "No results";
         }
     }
 
